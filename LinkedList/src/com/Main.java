1a5d7fabfec71583acdef01b2708022ff1ae7cc6
Naveen-2 <naveen2k15cse@gmail.com>
2022-03-06 19:09:56 +0530

    New linkedList will be created by adding 70 then 30 and 56 as in UC2 with 56 as head as final result

--- LinkedList/src/com/Main.java
+++ LinkedList/src/com/Main.java
@@ -1,11 +1,35 @@
 package com;
 
 public class Main {
-
+    /**
+     * Main method for manipulation linkedList
+     * @param args - Default Java param (Not used)
+     */
     public static void main(String[] args) {
+        /*
+         * PROCEDURE
+         * 1. Creating a linkedList
+         * 2. Adding nodes to linkedList
+         * 3. Adding nodes to linkedList in head
+         */
+
+        /*
+         * 1. Creating a linkedList
+         */
         LinkedList list = new LinkedList();
-        Node head = list.addNodeAsHead(null, 70);
-        head = list.addNodeAsHead(head, 30);
-        head = list.addNodeAsHead(head, 56);
+
+        /*
+         * 2. Adding nodes to linkedList as in UC1
+         */
+        Node head1 = list.addNode(null, 56);
+        list.addNode(head1, 30);
+        list.addNode(head1, 70);
+
+        /*
+         * 3. Adding nodes as head to the linkedList as in UC2
+         */
+        Node head2 = list.addNodeAsHead(null, 70);
+        head2 = list.addNodeAsHead(head2, 30);
+        head2 = list.addNodeAsHead(head2, 56);
     }
 }
