48e4c00781cf704d6b5005b29d18295b120810b4
Naveen-2 <naveen2k15cse@gmail.com>
2022-03-06 11:44:46 +0530

    A new linked list will be created and 3 nodes are added 1 after another

--- LinkedList/src/com/Main.java
+++ LinkedList/src/com/Main.java
@@ -1,10 +1,25 @@
 package com;
 
 public class Main {
-
+    /**
+     * Main method for manipulation linkedList
+     * @param args - Default Java param (Not used)
+     */
     public static void main(String[] args) {
-	// write your code here
+        /*
+         * PROCEDURE
+         * 1. Creating a linkedList
+         * 2. Adding nodes to linkedList
+         */
+
+        /*
+         * 1. Creating a linkedList
+         */
         LinkedList list = new LinkedList();
+
+        /*
+         * 2. Adding nodes to linkedList as in UC1
+         */
         Node head = list.addNode(null, 56);
         list.addNode(head, 30);
         list.addNode(head, 70);
